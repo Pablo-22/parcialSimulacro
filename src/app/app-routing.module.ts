e6b583af9eff42b9b8521eeb82f9723459e20a26
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-10-01 12:32:23 -0300

    Creación de módulos iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusquedaComponent } from './vistas/busqueda/busqueda.component';
+import { HomeComponent } from './vistas/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	
+	{ path: 'busqueda', component: BusquedaComponent },
+	{ path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './vistas/home/home.component';
+import { BusquedaComponent } from './vistas/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './peliculas/pelicula-alta/pelicula-alta.component';
+import { ActorAltaComponent } from './actores/actor-alta/actor-alta.component';
+import { PeliculaListadoComponent } from './peliculas/pelicula-listado/pelicula-listado.component';
+import { ActorListadoComponent } from './actores/actor-listado/actor-listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BusquedaComponent,
+    PeliculaAltaComponent,
+    ActorAltaComponent,
+    PeliculaListadoComponent,
+    ActorListadoComponent,
   ],
   imports: [
     BrowserModule,
